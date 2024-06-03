6efb87ca756a3a15c401872ceb36efe6e64e41e2
Paul <165521937+upcteus@users.noreply.github.com>
2024-06-03 00:46:51 -0500

    first commit
    first commit adding directories

---
+++ src/main/java/com/fivestars/rocketnotes/RocketnotesApplication.java
@@ -0,0 +1,13 @@
+package com.fivestars.rocketnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketnotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketnotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fivestars/rocketnotes/RocketnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivestars.rocketnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketnotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
