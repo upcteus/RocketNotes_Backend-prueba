d889809a280b6b0c26e8cb91c577a76af734bc02
Frida-Dam <u202218352@upc.edu.pe>
2024-06-07 22:47:00 -0500

    feat(profiles): added profile outbound service

--- src/main/java/com/fivestars/rocketnotes/profiles/domain/model/valueobjects/EmailAddress.java
+++ src/main/java/com/fivestars/rocketnotes/profiles/domain/model/valueobjects/EmailAddress.java
@@ -1,7 +1,7 @@
 package com.fivestars.rocketnotes.profiles.domain.model.valueobjects;
 
 import jakarta.persistence.Embeddable;
-
+import jakarta.validation.constraints.Email;
 
 @Embeddable
 public record EmailAddress(@Email String address) {
