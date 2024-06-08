1d47034d765caf0cf4327435637d1da4380234ea
upcteus <165521937+upcteus@users.noreply.github.com>
2024-06-08 01:24:14 -0500

    Update RocketnotesApplicationTests.java

--- src/test/java/com/fivestars/rocketnotes/RocketnotesApplicationTests.java
+++ src/test/java/com/fivestars/rocketnotes/RocketnotesApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
 @SpringBootTest
 @EnableJpaAuditing
 class RocketnotesApplicationTests {
-
     @Test
     void contextLoads() {
     }
